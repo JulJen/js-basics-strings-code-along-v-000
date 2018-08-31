7c54eda36b943d93dff9ab5e9c89baf7116c4c8d
Jul Jen <juliajen.prog@gmail.com>
2018-08-31 21:13:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const currentUser = 'string';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 const upperCaseCurrentUser = currentUser.toUpperCase();
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!';
-const shortGreeting = 'Welcome, ' +
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = 'Welcome, ';
