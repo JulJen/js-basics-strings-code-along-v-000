5f3f1d6d6ab86c743d6d2a0155ba49b63e13fd42
Jul Jen <juliajen.prog@gmail.com>
2018-08-31 21:14:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '
 
 const firstInitial = currentUser[0];
 const restOfName = currentUser.slice(1);
-const shortGreeting = 'Welcome, ';
+const shortGreeting = 'Welcome, ' + firstInitial + restOfName;
